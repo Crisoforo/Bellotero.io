8d5875cb37bdfe32eb08acbb91a7f3b587bd771e
Alejandro <alejandro.ortiz.pichardo@gmail.com>
2019-07-19 20:54:47 -0500

    Updates

---
+++ precache-manifest.5c006974184a1a6b35f11a039e9daeac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3dabc5663f2d97504aab662cf3614f",
+    "url": "/Bellotero.io/index.html"
+  },
+  {
+    "revision": "c99f33b0460a8279740a",
+    "url": "/Bellotero.io/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ca954b9b8bc59acf8c86",
+    "url": "/Bellotero.io/static/css/main.cb2d383d.chunk.css"
+  },
+  {
+    "revision": "c99f33b0460a8279740a",
+    "url": "/Bellotero.io/static/js/2.797a0f4d.chunk.js"
+  },
+  {
+    "revision": "ca954b9b8bc59acf8c86",
+    "url": "/Bellotero.io/static/js/main.04994e3e.chunk.js"
+  },
+  {
+    "revision": "ea8bd37fd222f4cb563c",
+    "url": "/Bellotero.io/static/js/runtime~main.c898cbeb.js"
+  }
+]);
